b133fc63b0e0bca523437049ddc4bb800bf7abfb
tan-sd <shengdatan@gmail.com>
2024-09-24 02:02:30 +0800

    Edit import statements

--- NGSI-LD-SG-Datamall/import_carpark.py
+++ NGSI-LD-SG-Datamall/import_carpark.py
@@ -1,5 +1,5 @@
-from libs.ura_parking import get_ura_token, get_carpark, get_season_carpark
-import libs.ngsi_ld_parking as ngsi_parking
+from mylibs.ura_parking import get_ura_token, get_carpark, get_season_carpark
+import mylibs.ngsi_ld_parking as ngsi_parking
 
 token = get_ura_token()['Result']
 
@@ -9,7 +9,7 @@ ngsi_parking.create_entities_in_broker(carpark_list)
 
 retrieved_carparks = ngsi_parking.retrieve_ngsi_type("Carpark")
 
-print(retrieved_carparks)
+# print(retrieved_carparks)
 print ("Num entities retrieved" , len(retrieved_carparks))
 for i in range(len(retrieved_carparks)):
     retrieved_carparks[i].pprint()
