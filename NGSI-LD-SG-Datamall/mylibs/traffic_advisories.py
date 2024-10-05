2ff62e5fee93ae73a020b894dec937f11cff0f2e
tan-sd <shengdatan@gmail.com>
2024-10-05 19:22:33 +0800

    Renamed to traffic_advisories

--- NGSI-LD-SG-Datamall/mylibs/traffic_advisories.py
+++ NGSI-LD-SG-Datamall/mylibs/traffic_advisories.py
@@ -3,19 +3,15 @@ import requests
 import json
 from ngsildclient import Client, Entity, SmartDataModels
 from datetime import datetime
-import mylibs.SVY21 as SVY21
 
 TRAFFICADVISORIES = "https://datamall2.mytransport.sg/ltaodataservice/VMS"
-ACCESS_KEY = constants.DATAMALL_ACCESS_KEY
+ACCESS_KEY = constants.DATAMALL_API_KEY
 
 ctx = constants.ctx
 broker_url = constants.broker_url
 broker_port = constants.broker_port # default, 80
 temporal_port = constants.temporal_port #default 1026
 broker_tenant = constants.broker_tenant
-
-# Initialise the SVY21 class
-svy21_converter = SVY21.SVY21()
 
 def get_traffic_advisories():
     response = requests.get(TRAFFICADVISORIES, headers={
@@ -43,7 +39,7 @@ def get_traffic_advisories():
             entity_list.append(entity)
         print("Total number of Traffic Advisories: ", len(traffic_advisories))
         print("Total entities created: ", len(entity_list))
-    
+
         return entity_list
     else:
         return None
