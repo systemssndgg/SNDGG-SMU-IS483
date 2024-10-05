4caa56e2cadf9679c2f41bbf0373c3b8f72a6850
tan-sd <shengdatan@gmail.com>
2024-10-05 19:21:18 +0800

    Delete file

--- NGSI-LD-SG-Datamall/import_weather_observed.py
+++
@@ -1,9 +0,0 @@
-from mylibs.weather_observed import get_weather_observed_data, create_entities_in_broker
-
-# Code to run ==================================================
-list_entities = get_weather_observed_data()
-
-print("Entities created: ", len(list_entities))
-print("\nPushing to broker...")
-
-create_entities_in_broker(list_entities)
